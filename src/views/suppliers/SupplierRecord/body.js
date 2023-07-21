0d27abd4f0172d1a5ed9f33bc0e6c530e8d2fa01
syeda339 <syedazainab339@outlook.com>
2023-07-21 05:14:28 +0100

    add supplier folder

---
+++ src/views/suppliers/SupplierRecord/body.js
@@ -0,0 +1,244 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import clsx from 'clsx';
+import * as Yup from 'yup';
+import PropTypes from 'prop-types';
+import { Formik } from 'formik';
+import { useSnackbar } from 'notistack';
+import {
+  Box,
+  Button,
+  TextField,
+  makeStyles
+} from '@material-ui/core';
+import axios from 'src/utils/axios';
+import { API_BASE_URL } from 'src/config';
+
+const useStyles = makeStyles(() => ({
+  root: {}
+}));
+
+function RegisterForm({ className, onSubmitSuccess, ...rest }) {
+  const classes = useStyles();
+  const { enqueueSnackbar } = useSnackbar();
+  const { user } = useSelector((state) => state.account);
+
+  return (
+    <Formik
+      initialValues={{
+        name: '',
+        mobile: '',
+        email: '',
+        phone: '',
+        country: '',
+        state: '',
+        city: '',
+        postcode: '',
+        address: '',
+      }}
+      validationSchema={Yup.object().shape({
+        name: Yup.string().max(255).required('Supplier Name is required'),
+        mobile: Yup.string().max(255).required('Supplier Mobile is required'),
+        email: Yup.string().max(255).required('Supplier Email is required'),
+        phone: Yup.string().max(255).required('Supplier Phone is required'),
+        country: Yup.string().max(255).required('Supplier Country is required'),
+        state: Yup.string().max(255).required('Supplier State is required'),
+        city: Yup.string().max(255).required('Supplier City is required'),
+        postcode: Yup.string().max(255).required('Supplier Postcode is required'),
+        address: Yup.string().max(255).required('Supplier Address is required'),
+      })}
+      onSubmit={async (values, {
+        setErrors,
+        setStatus,
+        setSubmitting
+      }) => {
+        try {
+          // Make API request
+          const user_id = user._id;
+          const { name, mobile, email, phone, country, state, city, postcode, address } = values;
+          var data = '';
+
+          await axios.post(API_BASE_URL + 'suppliers/create', { name, mobile, email, phone, country, state, city, postcode, address, user_id })
+            .then((response) => {
+              data = response.data;
+            })
+          if (data.success) {
+            setStatus({ success: true });
+            setSubmitting(false);
+            enqueueSnackbar(data.message, {
+              variant: 'success',
+            });
+          }
+          else {
+            enqueueSnackbar(data.message, {
+              variant: 'error'
+            });
+          }
+          onSubmitSuccess();
+        }
+        catch (error) {
+          setStatus({ success: false });
+          setErrors({ submit: error.message });
+          setSubmitting(false);
+        }
+      }}
+    >
+      {({
+        errors,
+        handleBlur,
+        handleChange,
+        handleSubmit,
+        isSubmitting,
+        touched,
+        values
+      }) => (
+        <form
+          className={clsx(classes.root, className)}
+          onSubmit={handleSubmit}
+          {...rest}
+        >
+          <TextField
+            error={Boolean(touched.name && errors.name)}
+            fullWidth
+            helperText={touched.name && errors.name}
+            label="Supplier Name"
+            margin="normal"
+            name="name"
+            onBlur={handleBlur}
+            onChange={handleChange}
+            type="string"
+            value={values.name}
+            variant="outlined"
+          />
+          <TextField
+            error={Boolean(touched.mobile && errors.mobile)}
+            fullWidth
+            helperText={touched.mobile && errors.mobile}
+            label="Mobile"
+            margin="normal"
+            name="mobile"
+            onBlur={handleBlur}
+            onChange={handleChange}
+            type="string"
+            value={values.mobile}
+            variant="outlined"
+          />
+          <TextField
+            error={Boolean(touched.email && errors.email)}
+            fullWidth
+            helperText={touched.email && errors.email}
+            label="Email"
+            margin="normal"
+            name="email"
+            onBlur={handleBlur}
+            onChange={handleChange}
+            type="string"
+            value={values.email}
+            variant="outlined"
+          />
+          <TextField
+            error={Boolean(touched.phone && errors.phone)}
+            fullWidth
+            helperText={touched.phone && errors.phone}
+            label="Phone"
+            margin="normal"
+            name="phone"
+            onBlur={handleBlur}
+            onChange={handleChange}
+            type="string"
+            value={values.phone}
+            variant="outlined"
+          />
+          <TextField
+            error={Boolean(touched.address && errors.address)}
+            fullWidth
+            helperText={touched.address && errors.address}
+            label="Address"
+            margin="normal"
+            name="address"
+            onBlur={handleBlur}
+            onChange={handleChange}
+            type="string"
+            value={values.address}
+            variant="outlined"
+          />
+          <TextField
+            error={Boolean(touched.city && errors.city)}
+            fullWidth
+            helperText={touched.city && errors.city}
+            label="City"
+            margin="normal"
+            name="city"
+            onBlur={handleBlur}
+            onChange={handleChange}
+            type="string"
+            value={values.city}
+            variant="outlined"
+          />
+          <TextField
+            error={Boolean(touched.state && errors.state)}
+            fullWidth
+            helperText={touched.state && errors.state}
+            label="State"
+            margin="normal"
+            name="state"
+            onBlur={handleBlur}
+            onChange={handleChange}
+            type="string"
+            value={values.state}
+            variant="outlined"
+          />
+          <TextField
+            error={Boolean(touched.postcode && errors.postcode)}
+            fullWidth
+            helperText={touched.postcode && errors.postcode}
+            label="Postcode"
+            margin="normal"
+            name="postcode"
+            onBlur={handleBlur}
+            onChange={handleChange}
+            type="string"
+            value={values.postcode}
+            variant="outlined"
+          />
+          <TextField
+            error={Boolean(touched.country && errors.country)}
+            fullWidth
+            helperText={touched.country && errors.country}
+            label="Country"
+            margin="normal"
+            name="country"
+            onBlur={handleBlur}
+            onChange={handleChange}
+            type="string"
+            value={values.country}
+            variant="outlined"
+          />
+          <Box mt={2}>
+            <Button
+              color="secondary"
+              disabled={isSubmitting}
+              fullWidth
+              size="large"
+              type="submit"
+              variant="contained"
+            >
+              Save
+            </Button>
+          </Box>
+        </form>
+      )}
+    </Formik>
+  );
+}
+
+RegisterForm.propTypes = {
+  className: PropTypes.string,
+  onSubmitSuccess: PropTypes.func
+};
+
+RegisterForm.default = {
+  onSubmitSuccess: () => { }
+};
+
+export default RegisterForm;
---
+++ src/views/suppliers/SupplierReport/body.js
@@ -0,0 +1,305 @@
+/* eslint-disable max-len */
+import React, { useState } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import clsx from 'clsx';
+import PropTypes from 'prop-types';
+import PerfectScrollbar from 'react-perfect-scrollbar';
+import {
+  Box,
+  Card,
+  IconButton,
+  SvgIcon,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TablePagination,
+  TableRow,
+  TextField,
+  colors,
+  makeStyles
+} from '@material-ui/core';
+import { Edit as EditIcon } from 'react-feather';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Label from 'src/components/Label';
+
+const statusOptions = [
+  {
+    id: 'all',
+    name: 'All'
+  },
+  {
+    id: 'active',
+    name: 'Active'
+  },
+  {
+    id: 'deactive',
+    name: 'Deactive'
+  },
+];
+
+
+function getStatusLabel(inventoryType) {
+  const map = {
+    active: {
+      text: 'Active',
+      color: 'success'
+    },
+    deactive: {
+      text: 'Deactive',
+      color: 'warning'
+    },
+  };
+
+  const { text, color } = map[inventoryType];
+
+  return (
+    <Label color={color}>
+      {text}
+    </Label>
+  );
+}
+
+function applyFilters(suppliers, filters) {
+  return suppliers.filter((supplier) => {
+    let matches = true;
+
+    if (filters.status && supplier.status !== filters.status) {
+      matches = false;
+    }
+
+    return matches;
+  });
+}
+
+function applyPagination(customers, page, limit) {
+  return customers.slice(page * limit, page * limit + limit);
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {},
+  queryField: {
+    width: 500
+  },
+  statusField: {
+    flexBasis: 200
+  },
+
+  avatar: {
+    backgroundColor: colors.red[500],
+    color: colors.common.white
+  }
+}));
+
+function Results({ className, suppliers, deleteSupplier, ...rest }) {
+  const classes = useStyles();
+  const [page, setPage] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const [filters, setFilters] = useState({
+    status: null
+  });
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleStatusChange = (event) => {
+    event.persist();
+
+    let value = null;
+
+    if (event.target.value !== 'all') {
+      value = event.target.value;
+    }
+
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      status: value
+    }));
+  };
+
+  const handleSearchTermChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handlePageChange = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleLimitChange = (event) => {
+    setLimit(event.target.value);
+  };
+
+  // Usually query is done on backend with indexing solutions
+  const filteredSuppliers = applyFilters(suppliers, filters);
+  const searchedSuppliers = filteredSuppliers.filter(item =>
+    item.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  const paginatedSuppliers = applyPagination(searchedSuppliers, page, limit);
+
+  return (
+    <Card
+      className={clsx(classes.root, className)}
+      {...rest}
+    >
+      <Box p={2}>
+        <Box
+          display="flex"
+          alignItems="center"
+        >
+          <TextField
+            className={classes.statusField}
+            label="Status"
+            name="status"
+            onChange={handleStatusChange}
+            select
+            SelectProps={{ native: true }}
+            value={filters.status || 'all'}
+            variant="outlined"
+          >
+            {statusOptions.map((statusOption) => (
+              <option
+                key={statusOption.id}
+                value={statusOption.id}
+              >
+                {statusOption.name}
+              </option>
+            ))}
+          </TextField>
+          <Box flexGrow={1} />
+          <TextField
+            label="Search"
+            variant="outlined"
+            value={searchTerm}
+            onChange={handleSearchTermChange}
+          />
+        </Box>
+      </Box>
+      <PerfectScrollbar>
+        <Box >
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>
+                  Supplier ID
+                </TableCell>
+                <TableCell>
+                  Name
+                </TableCell>
+                <TableCell>
+                  Mobile
+                </TableCell>
+                <TableCell>
+                  Email
+                </TableCell>
+                <TableCell>
+                  Phone
+                </TableCell>
+                <TableCell>
+                  Country
+                </TableCell>
+                <TableCell>
+                  State
+                </TableCell>
+                <TableCell>
+                  City
+                </TableCell>
+                <TableCell>
+                  Postcode
+                </TableCell>
+                <TableCell>
+                  Address
+                </TableCell>
+                <TableCell>
+                  Status
+                </TableCell>
+                <TableCell align="right">
+                  Actions
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {paginatedSuppliers.map((supplier) => {
+
+                return (
+                  <TableRow
+                    hover
+                    key={supplier._id}
+                  >
+                    <TableCell>
+                      {supplier.number}
+                    </TableCell>
+                    <TableCell>
+                      {supplier.name}
+                    </TableCell>
+                    <TableCell>
+                      {supplier.mobile}
+                    </TableCell>
+                    <TableCell>
+                      {supplier.email}
+                    </TableCell>
+                    <TableCell>
+                      {supplier.phone}
+                    </TableCell>
+                    <TableCell>
+                      {supplier.country}
+                    </TableCell>
+                    <TableCell>
+                      {supplier.state}
+                    </TableCell>
+                    <TableCell>
+                      {supplier.city}
+                    </TableCell>
+                    <TableCell>
+                      {supplier.postcode}
+                    </TableCell>
+                    <TableCell>
+                      {supplier.address}
+                    </TableCell>
+                    <TableCell>
+                      {getStatusLabel(supplier.status)}
+                    </TableCell>
+                    <TableCell align="right">
+                      <IconButton
+                        component={RouterLink}
+                        to={"/app/suppliers/supplierEdit/" + supplier._id}
+                      >
+                        <SvgIcon fontSize="small">
+                          <EditIcon />
+                        </SvgIcon>
+                      </IconButton>
+                      <IconButton
+                        onClick={() => { if (window.confirm('Are you really want to delete?')) deleteSupplier(supplier._id) }}
+                      >
+                        <SvgIcon fontSize="small">
+                          <DeleteIcon />
+                        </SvgIcon>
+                      </IconButton>
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </Box>
+      </PerfectScrollbar>
+      <TablePagination
+        component="div"
+        count={100}
+        onPageChange={handlePageChange}
+        onRowsPerPageChange={handleLimitChange}
+        page={page}
+        rowsPerPage={limit}
+        rowsPerPageOptions={[5, 10, 25]}
+      />
+    </Card>
+  );
+}
+
+Results.propTypes = {
+  className: PropTypes.string,
+  suppliers: PropTypes.array
+};
+
+Results.defaultProps = {
+  suppliers: []
+};
+
+export default Results;
